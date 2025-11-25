21e64fad203c40988cfc381e7aeb47146107dbcb
Lasya Indukuri <aravindalasya1@gmail.com>
2025-11-25 15:54:01 +0100

    ripple adder without latches source code is added

---
+++ projects/rippleAdder_timingErrors/rippleAdder_timingErrors.srcs/sim_1/new/ripple_adder_noLatches_tb.vhd
@@ -0,0 +1,99 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/25/2025 03:49:03 PM
+-- Design Name: 
+-- Module Name: ripple_adder_noLatches_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ripple_adder_noLatches_tb is
+--  Port ( );
+end ripple_adder_noLatches_tb;
+
+architecture Behavioral of ripple_adder_noLatches_tb is
+
+component ripple_adder_noLatches is
+    Port ( clk : in STD_LOGIC;
+           reset : in std_logic;
+           a : in STD_LOGIC_VECTOR (15 downto 0);
+           b : in STD_LOGIC_VECTOR (15 downto 0);
+           sum : out STD_LOGIC_VECTOR (15 downto 0));
+end component;
+
+signal clk,reset : std_logic := '0';
+signal a,b,sum : std_logic_vector(15 downto 0) := (others => '0');
+signal time_period : time := 10ns;
+
+begin
+
+UUT : ripple_adder_noLatches port map (a=>a, b=>b, clk=>clk, reset=>reset, sum=>sum);  
+
+clock_process : process
+begin
+    clk <= not clk;
+    wait for time_period/2;
+end process;
+
+input_process : process
+begin 
+    
+    reset <= '1';
+    a <= std_logic_vector(to_unsigned(16, 16));
+    b <= std_logic_vector(to_unsigned(16, 16));
+    
+    
+    wait for 10 * time_period;
+    reset <= '0';
+    wait for 1 * time_period;
+    reset <= '1';
+    
+    a <= std_logic_vector(to_unsigned(111, 16));
+    b <= std_logic_vector(to_unsigned(553, 16));
+    
+    
+    wait for 10 * time_period;
+    reset <= '0';
+    wait for 1 * time_period;
+    reset <= '1';
+    
+    a <= std_logic_vector(to_unsigned(4123, 16));
+    b <= std_logic_vector(to_unsigned(8191, 16));
+    
+    
+    wait for 10 * time_period;
+    reset <= '0';
+    wait for 1 * time_period;
+    reset <= '1';
+    
+    a <= std_logic_vector(to_unsigned(4339, 16));
+    b <= std_logic_vector(to_unsigned(7999, 16));
+    
+    wait;
+    
+end process;
+end Behavioral;
---
+++ projects/rippleAdder_timingErrors/rippleAdder_timingErrors.srcs/sources_1/new/ripple_adder_noLatches.vhd
@@ -0,0 +1,90 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/25/2025 03:18:46 PM
+-- Design Name: 
+-- Module Name: ripple_adder_noLatches - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ripple_adder_noLatches is
+    Port ( clk : in STD_LOGIC;
+           reset : in std_logic;
+           a : in STD_LOGIC_VECTOR (15 downto 0);
+           b : in STD_LOGIC_VECTOR (15 downto 0);
+           sum : out STD_LOGIC_VECTOR (15 downto 0));
+end ripple_adder_noLatches;
+
+architecture Behavioral of ripple_adder_noLatches is
+
+    signal s0,s1,s2,s3 : std_logic_vector(3 downto 0) := (others=> '0');
+    signal c0,c1,c2,c3 : unsigned(0 downto 0) := (others=> '0'); 
+    
+    signal s_s0,s_s1,s_s2 : std_logic_vector(3 downto 0) := (others=> '0');
+    signal s_c0,s_c1,s_c2,s_c3 : unsigned(0 downto 0) := (others=> '0'); 
+    
+begin
+P_COMB : process(a,b,s_c0, s_c1,s_c2)
+        variable tmp_sum1, tmp_sum2, tmp_sum3, tmp_sum4 : unsigned(4 downto 0) := (others=>'0');
+    begin   
+            tmp_sum1 := unsigned('0' & a(3 downto 0)) + unsigned('0' & b(3 downto 0));
+            c0(0) <= tmp_sum1(4);
+            s0 <= std_logic_vector(tmp_sum1(3 downto 0));
+            
+            tmp_sum2 := unsigned('0' & a(7 downto 4)) + unsigned('0' & b(7 downto 4)) + s_c0;
+            c1(0) <= tmp_sum2(4);
+            s1 <= std_logic_vector(tmp_sum2(3 downto 0));
+            
+            tmp_sum3 := unsigned('0' & a(11 downto 8)) + unsigned('0' & b(11 downto 8)) + s_c1;
+            c2(0) <= tmp_sum3(4);
+            s2 <= std_logic_vector(tmp_sum3(3 downto 0));
+            
+            tmp_sum4 := unsigned('0' & a(15 downto 12)) + unsigned('0' & b(15 downto 12)) + s_c2;
+            c3(0) <= tmp_sum4(4);
+            s3 <= std_logic_vector(tmp_sum4(3 downto 0));
+  
+            -- sum <= std_logic_vector(resize(c1, 16)); --& resize(c1, 4) & resize(c2, 4) & resize(c3, 4);      
+    end process P_COMB;
+    
+P_seq : process(clk) 
+begin
+        if rising_edge(clk) then
+            s_c0 <= c0;
+            s_c1 <= c1;
+            s_c2 <= c2;
+            s_c3 <= c3;
+            
+            s_s0 <= s0;
+            s_s1 <= s1;
+            s_s2 <= s2;
+            
+            sum <= s3 & s_s2 & s_s1 & s_s0;
+        end if;
+    end process P_seq;
+
+
+end Behavioral;
